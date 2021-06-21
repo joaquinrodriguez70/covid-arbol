f0d0454831f50e6843417a6f95e86c9e3aff6557
joaquinrodriguez70 <joaquinrodriguez70@gmail.com>
2021-06-21 09:51:48 -0500

    2021-06-21

---
+++ covid-arbol.bat
@@ -0,0 +1,3 @@
+del /Q C:\Users\joaqu\Documents\Mios2020\13-dev2020\covid19\covid-arbol\img 
+
+C:\Users\joaqu\Documents\Apps\rportable\R-Portable\App\R-Portable\bin\x64\Rscript.exe  --no-save  C:/Users/joaqu/Documents/Mios2020/13-dev2020/covid19/covid-arbol/dtreeCvidmx.r
